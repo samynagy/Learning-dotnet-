09b6f342837abf560fcafbfc6809eafeedac232f
Samy Nagy <samynagy95@gmail.com>
2026-09-26 12:05:14 +0300

    Add GET Genres Endpoint

---
+++ MoviesApi/MoviesApi/Controllers/GenresController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MoviesApi.Models;
+
+namespace MoviesApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GenresController : ControllerBase
+    {
+        private readonly ApplicationDBContext _context;
+        public GenresController(ApplicationDBContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllAsync()
+        {
+            var genres = await _context.Genres.ToListAsync();
+            return Ok(genres);
+        }
+
+    }
+}
