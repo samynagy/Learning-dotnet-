7064aa5aa5ee711faa17e210d24dc2d55fbfe8a9
Samy Nagy <samynagy95@gmail.com>
2026-09-26 11:53:48 +0300

    Create database and Genre model

--- MoviesApi/MoviesApi/Models/Genre.cs
+++ MoviesApi/MoviesApi/Models/Genre.cs
@@ -1,13 +1,16 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MoviesApi.Models
 {
     public class Genre
 
     {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public byte Id { get; set; }
 
         [Required]
+        [MaxLength(100)]
         public string Name { get; set; }
 
     }
