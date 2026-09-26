b71900ec7e4bb8b62d5d3ca3e810cdfbe326d556
Samy Nagy <samynagy95@gmail.com>
2026-09-26 17:54:22 +0300

    Add Movies Model

--- MoviesApi/MoviesApi/Migrations/20260926145350_addMovieTable.Designer.cs
+++ MoviesApi/MoviesApi/Migrations/20260926145350_addMovieTable.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MoviesApi.Models;
 
@@ -10,9 +11,11 @@ using MoviesApi.Models;
 namespace MoviesApi.Migrations
 {
     [DbContext(typeof(ApplicationDBContext))]
-    partial class ApplicationDBContextModelSnapshot : ModelSnapshot
+    [Migration("20260926145350_addMovieTable")]
+    partial class addMovieTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ MoviesApi/MoviesApi/Models/Movie.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MoviesApi.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        [MaxLength(250)]
+        public string Tilte { get; set; }
+
+        public int Year { get; set; }
+        public decimal Rate { get; set; }
+        [MaxLength(500)]
+        public string Storyline { get; set; }
+        public byte[] Poster { get; set; }
+
+
+        public byte GenreId { get; set; }
+        public Genre Genre { get; set; }
+
+
+    }
+}
